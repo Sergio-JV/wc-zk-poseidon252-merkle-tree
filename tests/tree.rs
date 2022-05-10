5aa05f6a801cf692f1dc02da80d78a594a964936
Sergio-JV <sergio@advancedblockchain.com>
2022-05-10 01:31:57 -0500

    The fisr commit with a modification for computing hash(r||s), then adding this to the tree

--- tests/tree.rs
+++ tests/tree.rs
@@ -16,10 +16,16 @@ use max_annotation::MockLeaf;
 use rand_core::{CryptoRng, OsRng, RngCore};
 
 use dusk_plonk::prelude::*;
+use dusk_poseidon::sponge;
+use crate::max_annotation::TornadoLeaf;
+
 
 const DEPTH: usize = 17;
 const CAPACITY: usize = 15;
 type Tree = PoseidonTree<MockLeaf, PoseidonAnnotation, DEPTH>;
+
+//This was added by me
+type Tree2 = PoseidonTree<TornadoLeaf, PoseidonAnnotation, DEPTH>;
 
 struct MerkleOpeningCircuit {
     branch: PoseidonBranch<DEPTH>,
@@ -76,23 +82,79 @@ impl Circuit for MerkleOpeningCircuit {
     }
 }
 
-#[test]
-fn tree_merkle_opening() {
-    let label = b"dusk-network";
-    let pp = PublicParameters::setup(1 << CAPACITY, &mut OsRng).unwrap();
 
-    let mut tree = Tree::default();
+    #[test]
+    fn tree_merkle_opening() {
+        let label = b"dusk-network";
+        let pp = PublicParameters::setup(1 << CAPACITY, &mut OsRng).unwrap();
 
-    let mut circuit = MerkleOpeningCircuit::random(&mut OsRng, &mut tree);
-    let (pk, vd) = circuit.compile(&pp).expect("Failed to compile circuit");
+        let mut tree = Tree::default();
 
-    let mut tree = Tree::default();
-    let mut circuit = MerkleOpeningCircuit::random(&mut OsRng, &mut tree);
+        let mut circuit = MerkleOpeningCircuit::random(&mut OsRng, &mut tree);
+        let (pk, vd) = circuit.compile(&pp).expect("Failed to compile circuit");
 
-    let proof = circuit
-        .prove(&pp, &pk, label)
-        .expect("Failed to generate proof");
+        //I do not know why this lines are repeated, so I have decided to comment those lines and the test
+        //still works, until now I id not see any reason why those line should be included
 
-    MerkleOpeningCircuit::verify(&pp, &vd, &proof, &[], label)
-        .expect("Proof verification failed");
-}
+        let mut tree = Tree::default();
+        let mut circuit = MerkleOpeningCircuit::random(&mut OsRng, &mut tree);
+
+        let proof = circuit
+            .prove(&pp, &pk, label)
+            .expect("Failed to generate proof");
+
+        MerkleOpeningCircuit::verify(&pp, &vd, &proof, &[], label)
+            .expect("Proof verification failed");
+    }
+
+
+
+    #[test]
+    fn tree_merkle_opening_r_s() {
+
+
+     let label = b"Test";
+     let public_params = PublicParameters::setup(1 << CAPACITY, &mut OsRng).unwrap();
+
+        let number_of_leaf = 1 << DEPTH;
+        // Create the ZK keys
+        let pp = PublicParameters::setup(1 << CAPACITY, &mut OsRng)
+            .expect("Failed generating the public parameters.");
+
+        let mut tree = Tree::default();
+        let mut circuit = MerkleOpeningCircuit::random(&mut OsRng, &mut tree);
+        let (pk, vd) = circuit.compile(&pp).expect("Failed to compile circuit");
+
+        // Instantiate a new tree
+        let mut tree: PoseidonTree<MockLeaf, PoseidonAnnotation, DEPTH> =
+            PoseidonTree::new();
+
+
+        let mut rng = OsRng;
+
+        // Append elements to the tree
+        for i in 0..number_of_leaf {
+            let r = BlsScalar::random(&mut rng);
+            let s = BlsScalar::random(&mut rng);
+
+            let r_s = sponge::hash(&[r, s]);
+
+            let s_r = sponge::hash(&[s,r]);
+
+            assert_ne!(r_s,s_r);
+
+            let l = MockLeaf::f_bls(r_s);
+
+            tree.push(l).expect("Failed appending to the tree");
+        }
+
+// Generate a ZK opening proof
+        let proof = circuit
+            .prove(&pp, &pk, label)
+            .expect("Failed to generate proof");
+
+// Verify the proof
+        MerkleOpeningCircuit::verify(&pp, &vd, &proof, &[], label)
+            .expect("Proof verification failed");
+    }
+
